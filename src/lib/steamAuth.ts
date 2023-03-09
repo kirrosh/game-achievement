33d1c420b3b6c35776ef970b5e09d9612bba9584
Koshelev Kirill <olegartor7@gmail.com>
2023-03-09 21:58:19 +0300

    Revert "fix: env"
    This reverts commit f4c45743986dfb46c37f9c7d8aeacc2f229c627d.

--- src/lib/steamAuth.ts
+++ src/lib/steamAuth.ts
@@ -1,7 +1,7 @@
 import SteamAuth from "node-steam-openid";
 
 export const steamAuth = new SteamAuth({
-  realm: `${process.env.VERCEL_URL}`,
-  returnUrl: `${process.env.VERCEL_URL}/api/trpc/session.authenticate`,
+  realm: `${process.env.DOMAIN}`,
+  returnUrl: `${process.env.DOMAIN}/api/trpc/session.authenticate`,
   apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`, // Steam API key
 });
